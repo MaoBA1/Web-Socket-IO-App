4fcdb56345194ebf77aeb1dbfc9746fb99ebe4dd
Maor Bar Asher <94516860+MaoBA1@users.noreply.github.com>
2023-01-28 18:06:39 +0200

    123

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { io } from 'socket.io-client';
+
+const socket = io.connect("http://localhost:3001");
 
 function App() {
+  const [ userName, setUserName ] = useState("");
+  const [ room, setRoom ] = useState("");
+
+  const joinRoom = () => {
+    if (userName !== "" && room !== "") {
+      
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Join A Chat</h3>
+      <input
+        type="text"
+        placeHolder="John..."
+        value={userName}
+        onChange={(event) => setUserName(event.target.value)}
+      />
+      <input
+        type="text"
+        placeHolder="Room Id..."
+        value={room}
+        onChange={(event) => setRoom(event.target.value)}
+      />
+      <button>Join A Room</button>
     </div>
   );
 }
